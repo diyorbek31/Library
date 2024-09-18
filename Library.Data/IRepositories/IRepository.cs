a98f866aa41e5a80b65fdad347c975723b976fd2
Diyorbek Juraev <85333075+diyorbek31@users.noreply.github.com>
2024-09-18 21:23:50 +0500

    Repositories has been added and databases created

---
+++ Library.Data/IRepositories/IRepository.cs
@@ -0,0 +1,11 @@
+﻿
+namespace Library.Data.IRepositories;
+
+public interface IRepository<TEntity>
+{
+    public Task<bool> InsertAsync(TEntity entity);
+    public Task<bool> UpdateAsync(TEntity entity);
+    public Task<bool> DeleteByIdAsync(int id);
+    public Task<TEntity> RetrievByIdAsync(int id);
+    public Task<List<TEntity>> RetrievAllAsync();
+}
