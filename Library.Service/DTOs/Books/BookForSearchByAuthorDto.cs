6b2f59fe8282de51f85e047a69292b3fcaea1741
Diyorbek Juraev <85333075+diyorbek31@users.noreply.github.com>
2024-09-19 20:09:29 +0500

    book service has been written

---
+++ Library.Service/DTOs/Books/BookForSearchByAuthorDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.Service.DTOs.Books
+{
+    internal class BookForSearchByAuthor
+    {
+    }
+}
