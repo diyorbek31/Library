f6d99b7cdf4c997a75e140e4746601703698b022
Diyorbek Juraev <85333075+diyorbek31@users.noreply.github.com>
2024-09-22 13:48:24 +0500

    Teacher Presentation done

--- Library.Domain/Entities/Student.cs
+++ Library.Domain/Entities/Student.cs
@@ -7,7 +7,7 @@ public class Student : Auditable
     public string FirstName {  get; set; }
     public string LastName { get; set; }
     public string Email { get; set; }
-    public int PhoneNumber {  get; set; }
+    public string PhoneNumber {  get; set; }
     public int Course { get; set; }
     public bool MembershipStatus {  get; set; }
 
--- Library.Service/DTOs/Students/StudentForUpdateDto.cs
+++ Library.Service/DTOs/Students/StudentForUpdateDto.cs
@@ -4,6 +4,6 @@ public class StudentForUpdateDto
 {
     public string FirstName { get; set; }
     public string LastName { get; set; }
-    public int PhoneNumber { get; set; }
+    public string PhoneNumber { get; set; }
     public bool MembershipStatus { get; set; }
 }
